e69c4608171964766b7a80ae97c560bfccc00a36
Muhannad <41810542+Muhannad-issa@users.noreply.github.com>
2018-12-25 23:42:09 -0500

    Update 3_basicApp.py

--- 3_basicApp.py
+++ 3_basicApp.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# we are mapping multiple URLs to the same function return value
+# mapping multiple URLs to the same function return value
 
 @app.route("/")
 @app.route("/<user>")
